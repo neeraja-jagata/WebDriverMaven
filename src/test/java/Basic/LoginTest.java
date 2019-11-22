c5d2b960c30faaecaf99884ba10cb169bc11251b
neeraja jagata <neeraja.jagata@gmail.com>
2019-11-22 12:39:43 -0600

    Initial Commit

---
+++ src/test/java/Basic/LoginTest.java
@@ -0,0 +1,34 @@
+package Basic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		
+	}
+	@Test
+	public void login() {
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("neerujagata@gmail.com"+Keys.ENTER);
+		System.out.println(driver.getTitle());
+	}
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+		
+	}
+	
+}
